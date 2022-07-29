32361ad704969b6ff7dbd028458eb29dc6b71987
arantxaherranz <arantxah@gmail.com>
2022-07-29 12:16:45 +0200

    add dengue data and ebola plot

--- data/Dengue/dengue_americas.R
+++ data/Dengue/dengue_americas.R
@@ -12,9 +12,10 @@ world <- map_data("world")
 world <- world[world$region != "Antarctica",]
 world
 mapdata <- map_data("world")
+map_data
 
 mapdata1 <- mapdata %>% filter(!is.na(mapdata$X2020))
-view(mapdata1)
+mapdata1
 
 map20 <- ggplot(mapdata1, aes( x = long, y = lat, group=group))+
   geom_polygon(aes(fill = X2020), color = "black") +
